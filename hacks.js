301abac002ada28af2270a779cfeeaeff52a9259
Dragon_X <barron.samuel111@gmail.com>
2024-10-06 12:43:05 -0700

    Update hacks.js

--- hacks.js
+++ hacks.js
@@ -1,32 +1,32 @@
 [
-    {
-      "hack": {
-        "name": "Hello World",
-        "description": "Says Hello World",
-        "section": "Example",
-        "dist": "(async()=>{console.log(\"Hello World\")})();",
-        "success": "It says Hello world in console",
-        "developing": "false"
-      }
-    },
-    {
-      "hack": {
-        "name": "Set Level",
-        "description": "Sets the level of the player.",
-        "section": "Player",
-        "dist": "console.log('Added PXI Fusion Menu To ProdigyMOD')",
-        "success": "Player level set successfully.",
-        "developing": "true"
-      }
-    },
-    {
-        "hack": {
-          "name": "Test hack",
-          "description": "This does nothing.",
-          "section": "Player",
-          "dist": "console.log('1234')",
-          "success": "Nothing happened.",
-          "developing": "false"
-        }
+  {
+    "hack": {
+      "name": "Hello World",
+      "description": "Says Hello World",
+      "section": "Example",
+      "dist": "(async()=>{console.log('Hello World')})();",
+      "success": "It says Hello world in console",
+      "developing": "false"
     }
-];
+  },
+  {
+    "hack": {
+      "name": "Set Level",
+      "description": "Sets the level of the player.",
+      "section": "Player",
+      "dist": "console.log('Added PXI Fusion Menu To ProdigyMOD')",
+      "success": "Player level set successfully.",
+      "developing": "true"
+    }
+  },
+  {
+    "hack": {
+      "name": "Test hack",
+      "description": "This does nothing.",
+      "section": "Player",
+      "dist": "console.log('1234')",
+      "success": "Nothing happened.",
+      "developing": "false"
+    }
+  }
+]
